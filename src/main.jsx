e27f2e5be6499ac16b1f33d79e1d973f936cd992
SAC <sac.673@gmail.com>
2023-06-10 17:11:02 +0200

    son of page load control

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,23 @@ import App from './App_r.jsx'
 // import './index.css'
 // import './App_r.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-  <div id="dummy" className="placeholder deferred"></div>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <>
+//   <div id="dummy" className="placeholder deferred"></div>
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//   </>
+// )
+
+// this is because the load was terrible looking.
+document.addEventListener("DOMContentLoaded", function(event) {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+    <div id="dummy" className="placeholder deferred"></div>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    </>
+  );
+});
