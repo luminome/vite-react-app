9c0a0c775b316be69d57df88d4ffbb7d75f03921
SAC <sac.673@gmail.com>
2023-06-10 17:02:34 +0200

    tyr the previous thing again

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App_r.jsx'
-import './index.css'
-import './App_r.css'
+// import './index.css'
+// import './App_r.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
